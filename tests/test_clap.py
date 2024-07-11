423d8102a854b9365667f49c7696361a2529405b
Evangelos Lamprou <vagos@lamprou.xyz>
2024-07-11 02:17:08 +0200

    Change test model

--- tests/test_clap.py
+++ tests/test_clap.py
@@ -2,7 +2,7 @@ import llm
 
 
 def test_run_embedding():
-    model = llm.get_embedding_model("clip")
+    model = llm.get_embedding_model("clap")
     result = model.embed("bunny")
     assert len(result) == 512
     assert isinstance(result[0], float)
